2ae8941aa008b3972332b354bdfbed418b6387fc
mizaeldouglas <mizaeldouglas2@gmail.com>
2023-06-22 21:04:51 -0300

    adicionado OpenIA

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClientController;
+use OpenAI\Laravel\Facades\OpenAI;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +19,25 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('/clients', ClientController::class);
+
+    Route::get('/chart', function (){
+      return OpenAI::completions()->create([
+        'model' => 'text-davinci-003',
+        'prompt' => 'me de uma classe controller resource no padrao laravel',
+        'max_tokens' => 1500
+      ])->choices[0]->text;
+    });
+});
+
+require __DIR__.'/auth.php';
